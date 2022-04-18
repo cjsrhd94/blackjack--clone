c0eb79367725b6b520825819f5a56fb9cdee621d
cjsrhd94 <cjsrhd94@gmail.com>
2022-04-18 21:36:38 +0900

    feat: Game 모델 phase메서드에 dealer 행위  추가
    1. 각각의 pahse 마다 dealer가 하는 행위들이 추가되었습니다.

--- src/main/java/blackjack/domain/Game.java
+++ src/main/java/blackjack/domain/Game.java
@@ -3,40 +3,62 @@ package blackjack.domain;
 import java.util.Scanner;
 
 public class Game {
+
+    private static final int INIT_DRAW_CARD_COUNT = 2;
+
     public void play() {
         System.out.println("=====BlackJack=====");
         Scanner sc = new Scanner(System.in);
 
-//        Dealer dealer = new Dealer();
+        Dealer dealer = new Dealer();
         Player player = new Player();
 //        Rule rule = new Rule();
         CardDeck cardDeck = new CardDeck();
 
-        initPhase(cardDeck, player);
-        playingPhase(sc, cardDeck, player);
+        initPhase(cardDeck, player, dealer);
+        playingPhase(sc, cardDeck, player, dealer);
     }
 
-    private static final int INIT_DRAW_CARD_COUNT = 2;
-    private void initPhase(CardDeck cardDeck, Player player) {
+    private void initPhase(CardDeck cardDeck, Player player, Dealer dealer) {
         System.out.println("2장의 카드를 뽑겠습니다.");
         for (int i = 0; i < INIT_DRAW_CARD_COUNT; i++) {
             Card card = cardDeck.draw();
             player.drawCard(card);
+
+            Card card2 = cardDeck.draw();
+            dealer.drawCard(card2);
         }
     }
 
-    private void playingPhase(Scanner sc, CardDeck cardDeck, Player player) {
-        String gamerInput;
+    private void playingPhase(Scanner sc, CardDeck cardDeck, Player player, Dealer dealer) {
+        String gamerInput, dealerInput;
+        boolean isPlayerTurn = false;
+        boolean isDealerTurn = false;
+
         while (true) {
             System.out.println("카드를 뽑으시겠습니까? 종료하시려면 0을 입력하세요.");
             gamerInput = sc.nextLine();
 
             if ("0".equals(gamerInput)) {
-                break;
+                isPlayerTurn = true;
+            } else {
+                Card card = cardDeck.draw();
+                player.drawCard(card);
             }
 
-            Card card = cardDeck.draw();
-            player.drawCard(card);
+            System.out.println("카드를 뽑으시겠습니까? 종료하시려면 0을 입력하세요.");
+            dealerInput = sc.nextLine();
+
+            if ("0".equals(dealerInput)) {
+                isDealerTurn = true;
+            } else {
+                Card card = cardDeck.draw();
+                dealer.drawCard(card);
+            }
+
+            if (isPlayerTurn && isDealerTurn) {
+                break;
+            }
         }
     }
 }
